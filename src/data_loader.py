2e6ec63f27a98ea9a643850936d930c4eac10af5
mayankbisht-tech <mayankbisht1107@gmail.com>
2026-06-24 12:07:07 +0000

    add rag

---
+++ src/data_loader.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+from typing import List, Dict, Any
+from langchain_community.document_loaders import PyPDFLoader,TextLoader,CSVLoader
+from langchain_community.document_loaders.excel import UnstructuredExcelLoader
+from langchain_community.document_loaders import JSONLoader
+
